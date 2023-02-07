f9ca1a73538ba31dbd2d814d2df93c0c8a36d67c
Gabriel Dias <gaabrieldls@gmail.com>
2023-02-06 16:28:01 -0800

    feature: final push

---
+++ app/src/main/java/com/example/eventhandlingmethodone/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.eventhandlingmethodone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ConstraintLayout constraintLayout;
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById((R.id.constraintLayout));
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                constraintLayout.setBackgroundColor(Color.CYAN);
+                Log.e(TAG, "Color changed to Cyan");
+                break;
+
+                case R.id.button2:
+                    constraintLayout.setBackgroundColor(Color.YELLOW);
+                    Log.e(TAG, "Color changed to Yellow");
+                    break;
+
+        }
+    }
+}
